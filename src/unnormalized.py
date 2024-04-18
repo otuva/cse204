579a0c0d2223f0281385c12aa1ce467786ad9d95
ONUR ALP AKIN <onur.alp.akin@proton.me>
2024-04-19 01:22:48 +0300

    modify unnormlz

--- src/unnormalized.py
+++ src/unnormalized.py
@@ -4,32 +4,36 @@ import csv
 
 from src import fields
 
+FILENAME = "unnormalized.csv"
 
-def generate_people(total_people):
+
+def insert_people_into_csv(total_people):
     URL = f"https://randomuser.me/api/?results={total_people}&seed=cse204"
 
     content = requests.get(URL).content
-    results = json.loads(content)["results"]
+    people = json.loads(content)["results"]
 
-    return results
-
-
-def insert_into_csv(people):
-    with open("people.csv", "w", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields.fieldnames)
-        writer.writeheader()
+    with open(FILENAME, "a", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields.person_fieldnames)
         for person in people:
             person_data = fields.person_data(person)
             writer.writerow(person_data)
 
 
+def create_csv_file():
+    with open(FILENAME, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields.fieldnames)
+        writer.writeheader()
+
+
 def get_from_csv():
-    with open("people.csv", newline="") as csvfile:
+    with open(FILENAME, newline="") as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=fields.fieldnames)
         people = [row for row in reader]
     return people
 
 
+# function to call from main.py
 def create_unnormalized(total_people):
-    people = generate_people(total_people)
-    insert_into_csv(people)
+    create_csv_file()
+    insert_people_into_csv(total_people)
