e05a2a2ed04bcb2d0a29ce4285e31f9cdd371ec5
ONUR ALP AKIN <onur.alp.akin@proton.me>
2024-04-19 02:53:24 +0300

    use random for genetaing hotels

--- src/unnormalized.py
+++ src/unnormalized.py
@@ -1,22 +1,48 @@
 import requests
 import json
 import csv
+import random
 
 from src import fields
+from src import filler
 
 FILENAME = "unnormalized.csv"
+random.seed("cse204 is the best lecture ever")
 
+existing_rooms = []
 
-def insert_people_into_csv(total_people):
+def generate_people(total_people):
     URL = f"https://randomuser.me/api/?results={total_people}&seed=cse204"
 
     content = requests.get(URL).content
-    people = json.loads(content)["results"]
+    return json.loads(content)["results"]
+
+
+def generate_hotel():
+    random_hotel = random.choice(filler.hotels)
+
+    random_room_number = random.randint(100,500)
+    while random_room_number in existing_rooms:
+        random_room_number = random.randint(100,500)
+    existing_rooms.append(random_room_number)
+
+    random_room_type = random.choice(filler.room_types)
+    random_payment_type = random.choice(filler.payment_types)
+    random_review = random.choice(filler.reviews)
+
+    return fields.hotel_data(
+        random_hotel, random_room_number, random_room_type, random_payment_type, random_review
+    )
+
+
+def insert_into_csv(total_people):
+    people = generate_people(total_people)
 
     with open(FILENAME, "a", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields.person_fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames=fields.fieldnames)
         for person in people:
             person_data = fields.person_data(person)
+            person_data.update(generate_hotel())
             writer.writerow(person_data)
 
 
@@ -36,4 +62,4 @@ def get_from_csv():
 # function to call from main.py
 def create_unnormalized(total_people):
     create_csv_file()
-    insert_people_into_csv(total_people)
+    insert_into_csv(total_people)
