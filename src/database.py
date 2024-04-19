1c77cc16450b2ac7bcb39d7c840d920f82342d94
ONUR ALP AKIN <onur.alp.akin@proton.me>
2024-04-19 19:15:17 +0300

    insert from csv to unf

--- src/database.py
+++ src/database.py
@@ -10,6 +10,8 @@ def create_tables():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
 
+    cursor.execute(fields.database_table_addresses)
+    cursor.execute(fields.database_table_contacts)
     cursor.execute(fields.database_table_guests)
     cursor.execute(fields.database_table_hotels)
     cursor.execute(fields.database_table_room_types)
@@ -24,62 +26,27 @@ def create_tables():
     conn.close()
 
 
+def insert_unnormalized_form(row):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    columns = ", ".join(fields.fieldnames)
+    placeholders = ", ".join("?" * len(fields.fieldnames))
+
+    # print(
+    cursor.execute(
+        f"""
+        INSERT INTO unnormalized_form ({columns})
+        VALUES ({placeholders})
+        """,
+        tuple(row.values()),
+    )
+
+    conn.commit()
+    conn.close()
+
+
 def insert_from_csv():
     people = unnormalized.get_from_csv()
     for person in people:
-        # insert_guest(person)
-        print(person)
-
-
-# def insert_guest(person):
-#     conn = sqlite3.connect(DATABASE)
-#     cursor = conn.cursor()
-
-#     cursor.execute(
-#         """
-#         INSERT INTO guests (gender, title, first_name, last_name, street_number, street_name,
-#                            city, state, country, postcode, latitude, longitude, timezone_offset,
-#                            timezone_description, email, uuid, username, password, salt, md5,
-#                            sha1, sha256, date_of_birth, age, registration_date, phone, cell,
-#                            id_name, id_value, picture_large, picture_medium, picture_thumbnail, nationality)
-#         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-#     """,
-#         (
-#             person["gender"],
-#             person["name"]["title"],
-#             person["name"]["first"],
-#             person["name"]["last"],
-#             person["location"]["street"]["number"],
-#             person["location"]["street"]["name"],
-#             person["location"]["city"],
-#             person["location"]["state"],
-#             person["location"]["country"],
-#             person["location"]["postcode"],
-#             person["location"]["coordinates"]["latitude"],
-#             person["location"]["coordinates"]["longitude"],
-#             person["location"]["timezone"]["offset"],
-#             person["location"]["timezone"]["description"],
-#             person["email"],
-#             person["login"]["uuid"],
-#             person["login"]["username"],
-#             person["login"]["password"],
-#             person["login"]["salt"],
-#             person["login"]["md5"],
-#             person["login"]["sha1"],
-#             person["login"]["sha256"],
-#             person["dob"]["date"],
-#             person["dob"]["age"],
-#             person["registered"]["date"],
-#             person["phone"],
-#             person["cell"],
-#             person["id"]["name"],
-#             person["id"]["value"],
-#             person["picture"]["large"],
-#             person["picture"]["medium"],
-#             person["picture"]["thumbnail"],
-#             person["nat"],
-#         ),
-#     )
-
-#     conn.commit()
-#     conn.close()
+        insert_unnormalized_form(person)
